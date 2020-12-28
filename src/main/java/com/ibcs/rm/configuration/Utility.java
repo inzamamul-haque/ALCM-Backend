70d50fa785120f2a193e768c5c6044b67f360e75
monirozzaman <moniruzzaman35-223@diu.edu.bd>
2020-12-28 12:17:14 +0600

    Add Utility Class

---
+++ src/main/java/com/ibcs/rm/configuration/Utility.java
@@ -0,0 +1,10 @@
+package com.ibcs.rm.configuration;
+
+import java.util.UUID;
+
+public class Utility {
+    public static String getRandomUuid(){
+        return UUID.randomUUID().toString();
+    }
+
+}
